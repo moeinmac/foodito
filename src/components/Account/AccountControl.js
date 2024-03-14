fa70a0a92df9f2548d3101c20ec944f37b465271
Moein.MAC <moein.mac@gmail.com>
2024-03-14 11:55:18 +0330

    toggleEditing

--- src/components/Account/AccountControl.js
+++ src/components/Account/AccountControl.js
@@ -1,27 +1,44 @@
-import { Link } from "react-router-dom";
 import styles from "./AccountControl.module.css";
 import EditAccount from "./EditAccount";
+import Logout from "./Logout";
+import Button from "../../UI/Button";
 
 import { RxCaretRight } from "react-icons/rx";
 import { IoIosLogOut } from "react-icons/io";
 
-import Logout from "./Logout";
+import { Link } from "react-router-dom";
 import MediaQuery from "react-responsive";
+import { useSelector, useDispatch } from "react-redux";
+import editSlice from "../../store/editSlice";
 
 const AccountControl = () => {
+  const edit = useSelector((state) => state.edit);
+  const dispatch = useDispatch();
+  const discardHandler = () => dispatch(editSlice.actions.toggleEditing());
+
+  if (edit.Editing) {
+    return (
+      <div className={styles.editing}>
+        <Button className={styles.discardButton} onClick={discardHandler}>
+          Discard
+        </Button>
+        <Button>Save Changes</Button>
+      </div>
+    );
+  }
   return (
     <div className={styles.container}>
       <EditAccount className={styles.item}>
-        <RxCaretRight className={styles.icon}/>
+        <RxCaretRight className={styles.icon} />
       </EditAccount>
 
       <MediaQuery maxWidth={600}>
-      <Link className={styles.item} to="/">
-        Manage Orders <RxCaretRight className={styles.icon}/>
-      </Link>
-      <Logout className={styles.item}>
-        <IoIosLogOut className={styles.icon}/>
-      </Logout>
+        <Link className={styles.item} to="/">
+          Manage Orders <RxCaretRight className={styles.icon} />
+        </Link>
+        <Logout className={styles.item}>
+          <IoIosLogOut className={styles.icon} />
+        </Logout>
       </MediaQuery>
     </div>
   );
--- src/components/Account/EditAccount.js
+++ src/components/Account/EditAccount.js
@@ -1,6 +1,12 @@
+import { useDispatch } from "react-redux";
+import editSlice from "../../store/editSlice";
+
 const EditAccount = (props) => {
+  const dispatch = useDispatch();
+  const editingHandler = () => dispatch(editSlice.actions.toggleEditing());
+  
   return (
-    <div className={props.className}>
+    <div className={props.className} onClick={editingHandler}>
       Edit Account {props.children}
     </div>
   );
