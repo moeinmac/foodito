337c464a30c1f3590b88185761c12b769c785bac
Moein.MAC <moein.mac@gmail.com>
2024-03-15 13:38:08 +0330

    redirect to / not auth

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -4,9 +4,17 @@ import AccountHeader from "../components/Account/AccountHeader";
 import MediaQuery from "react-responsive";
 import AccountMain from "../components/Account/AccountMain";
 import AccountControl from "../components/Account/AccountControl";
-import styles from './Account.module.css'
+import styles from './Account.module.css';
+
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Account = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if(!localStorage.getItem("supabase.auth.token")) navigate("/")
+  }, []);
+
   return (
     <div className={styles.account}>
       <AccountHeader />
