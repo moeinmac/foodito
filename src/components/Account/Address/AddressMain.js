fe9c7b2a99cdbbcd810eead6d16f75023fcf8648
Moein.MAC <moein.mac@gmail.com>
2024-03-16 20:40:29 +0330

    new style to address

--- src/components/Account/Address/AddressMain.js
+++ src/components/Account/Address/AddressMain.js
@@ -5,9 +5,18 @@ import EditAddress from "./EditAddress";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-const AddressMain = () => {
-  const address = useSelector((state) => state.user.user.address);
+const addressMaker = (address)=> {
+  let addressValue = ""
+  for (const item in address) {
+    if(address[item]) addressValue += `${item} : ${address[item]} , `
+  }
+  addressValue = addressValue.split(",")
+  return addressValue.splice(0,addressValue.length - 1).join(",")
+}
 
+const AddressMain = () => {
+
+  const address = useSelector((state) => state.user.user.address);
   const [editAddress, setEditAddress] = useState();
   const editAddressHandler = () => setEditAddress(!editAddress);
 
@@ -28,11 +37,10 @@ const AddressMain = () => {
   }
   return (
     <div className={styles.container}>
-      {Object.keys(address).map((item) => (
-        <p key={item} className={styles.item}>
-          {item} : {address[item]}
-        </p>
-      ))}
+      <p className={styles.item}>
+        {addressMaker(address)}
+      </p>
+      <img src="https://cgrqgisynruhcspvtobo.supabase.co/storage/v1/object/public/food/address.svg"/>
       <div className={styles.editButton}>
         <Link to="/account">Back</Link>
         <Button onClick={editAddressHandler}>Edit Address</Button>
